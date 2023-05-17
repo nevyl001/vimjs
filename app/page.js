e168e3226060c80301a0a4eb866ecefcfb1ed133
Nevyl <christiannevyl@MacBook-Pro-de-ChristianNevyl.local>
2023-05-17 13:19:36 -0600

    todaysCommit1.20

--- app/page.js
+++ app/page.js
@@ -114,7 +114,7 @@ export default function Home() {
               animate={{ opacity: 1 }}
               transition={{ duration: 0.75, delay: 1 }}
             >
-              <div className="scroll">
+              <div className="scroll scroll-desktop">
                 <p>Scroll down para ver proyectos</p>
                 <div className="down">
                   <svg
@@ -126,6 +126,23 @@ export default function Home() {
                   >
                     <path
                       fillRule="evenodd"
+                      d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="scroll scroll-mobile">
+                <div className="circle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30px"
+                    height="30px"
+                    fill="currentColor"
+                    class="bi bi-arrow-down"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
                       d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
                     />
                   </svg>
